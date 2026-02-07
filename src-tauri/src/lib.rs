ac10dcac3ddd5caeb67e4da29f994664bfc28386
Remi Marche <remimarche@gmail.com>
2026-02-06 23:44:56 -0500

    feat: add window state plugin and update dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -37,6 +37,7 @@ fn add_file_command(path: String) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![

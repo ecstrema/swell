3c38fd8575433178719c2818406b954d328f1923
Remi Marche <remimarche@gmail.com>
2026-02-17 21:44:29 -0500

    try to fix menu bar not appearing

--- src/components/app-main/app-main.ts
+++ src/components/app-main/app-main.ts
@@ -3,11 +3,8 @@ import { CommandManager } from "../command/command-manager.js";
 import { css } from "../../utils/css-utils.js";
 import appMainCss from "./app-main.css?inline";
 import { DockManager } from "../../extensions/dock-extension/dock-manager.js";
-import { Extension } from "../../extensions/types.js";
-
-// Setting paths
-const SETTING_NETLIST_VISIBLE = 'Interface/Netlist Visible';
-const SETTING_UNDO_HISTORY_VISIBLE = 'Interface/Undo History Visible';
+import { MenuBar } from "../../extensions/menu-extension/menu-bar.js";
+import { MenuExtension } from "../../extensions/menu-extension/menu-extension.js";
 
 export class AppMain extends HTMLElement {
     private commandManager: CommandManager;
@@ -33,17 +30,21 @@ export class AppMain extends HTMLElement {
         await extensionRegistry.register(CoreUIExtension);
 
         // Initialize the dock system
-        // The dock manager element is in the shadow DOM, created by the template
         const dockManagerElement = this.shadowRoot!.getElementById('main-dock') as DockManager;
-
-        // Get the dock extension API
-        // It returns the extension instance which implements the API
         const dockExtension = await extensionRegistry.getExtension<any>('core/dock');
-
         if (dockExtension && typeof dockExtension.initializeDockSystem === 'function') {
             dockExtension.initializeDockSystem(dockManagerElement);
         } else {
             console.error('Failed to initialize dock system: Dock extension not found or invalid API');
+        }
+
+        // Set up the menu bar
+        const menuBarElement = this.shadowRoot!.querySelector('app-menu-bar') as MenuBar;
+        const menuExtension = await extensionRegistry.getExtension<MenuExtension>('core/menu');
+        if (menuExtension && typeof menuExtension.setMenuBar === 'function') {
+            menuExtension.setMenuBar(menuBarElement);
+        } else {
+            console.error('Failed to initialize menu bar: Menu extension not found or invalid API');
         }
     }
 
